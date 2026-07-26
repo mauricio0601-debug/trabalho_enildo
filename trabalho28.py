99cffcd8f4d7c382b1276e7cce17b49a659372c0
mauricio <mauriciomarangoni0601@gmail.com>
2026-07-26 13:11:02 -0300

    modificando algumas quebras de linha para melhor visualização

--- trabalho28.py
+++ trabalho28.py
@@ -7,7 +7,7 @@ ARQUIVO = "alunos.txt"
 
 
 def titulo(texto):
-    print(Fore.CYAN + "\n" + "=" * 70)
+    print(Fore.BLUE + "\n" + "=" * 70)
     print(texto.center(70))
     print("=" * 70 + Style.RESET_ALL)
 
@@ -16,32 +16,32 @@ def cadastrar():
     titulo("CADASTRAR ALUNO")
 
     while True:
-        nome = str(input(Fore.LIGHTBLACK_EX + "DIGITE O NOME DO ALUNO(A): " + Style.RESET_ALL))
+        nome = str(input(Fore.LIGHTBLACK_EX + "DIGITE O NOME DO ALUNO(A): \n" + Style.RESET_ALL))
         if nome.replace(" ", "").isalpha():
             break
         elif nome ==" ":
-            print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS" + Style.RESET_ALL)
+            print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS\n" + Style.RESET_ALL)
         else:
-            print(Fore.YELLOW + "DIGITE APENAS LETRAS" + Style.RESET_ALL)
+            print(Fore.YELLOW + "DIGITE APENAS LETRAS\n" + Style.RESET_ALL)
 
     while True:
         try:
-            nota = float(input(Fore.LIGHTBLACK_EX + "DIGITE A NOTA DO ALUNO(A): " + Style.RESET_ALL).replace(",",".")) 
+            nota = float(input(Fore.LIGHTBLACK_EX + "DIGITE A NOTA DO ALUNO(A): \n" + Style.RESET_ALL).replace(",",".")) 
             
         
             if 0 <= nota <= 10:
                 break
             elif nota ==" ":
-                print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS" + Style.RESET_ALL)
+                print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS\n" + Style.RESET_ALL)
             else:
-                print(Fore.YELLOW + "DIGITE APENAS NÚMEROS ENTRE 0 E 10" + Style.RESET_ALL)
+                print(Fore.YELLOW + "DIGITE APENAS NÚMEROS ENTRE 0 E 10\n" + Style.RESET_ALL)
         except ValueError:
-            print(Fore.YELLOW + "DIGITE APENAS NÚMEROS")
+            print(Fore.YELLOW + "DIGITE APENAS NÚMEROS\n")
 
     with open(ARQUIVO, "a", encoding="utf-8") as arquivo:
         arquivo.write(f"{nome};{nota}\n")
 
-    print(Fore.GREEN + "Aluno(a) cadastrado com sucesso!" + Style.RESET_ALL)
+    print(Fore.GREEN + "Aluno(a) cadastrado com sucesso!\n" + Style.RESET_ALL)
 
 
 def listar():
@@ -52,14 +52,14 @@ def listar():
     print("-" * 70)
 
     if not os.path.exists(ARQUIVO):
-        print(Fore.RED + "Arquivo de cadastro inexistente!" + Style.RESET_ALL)
+        print(Fore.RED + "Arquivo de cadastro inexistente!\n" + Style.RESET_ALL)
         return
 
     with open(ARQUIVO, "r", encoding="utf-8") as arquivo:
         alunos = arquivo.readlines()
 
     if len(alunos) == 0:
-        print(Fore.YELLOW + "Nenhum aluno cadastrado." + Style.RESET_ALL)
+        print(Fore.YELLOW + "Nenhum aluno cadastrado.\n" + Style.RESET_ALL)
         return
 
     for linha in alunos:
@@ -83,10 +83,10 @@ def alterar():
     titulo("ALTERAR ALUNO(A)")
 
     if not os.path.exists(ARQUIVO):
-        print(Fore.RED + "Arquivo de cadastro inexistente!" + Style.RESET_ALL)
+        print(Fore.RED + "Arquivo de cadastro inexistente!\n" + Style.RESET_ALL)
         return
 
-    aluno = input(Fore.BLUE + "Digite o nome do aluno(a): " + Style.RESET_ALL).strip()
+    aluno = input(Fore.BLUE + "Digite o nome do aluno(a): \n" + Style.RESET_ALL).strip()
 
     with open(ARQUIVO, "r", encoding="utf-8") as arquivo:
         alunos = arquivo.readlines()
@@ -100,51 +100,51 @@ def alterar():
         if nome.lower() == aluno.lower():
             encontrado = True
             while True:
-                novo_nome = input(Fore.LIGHTBLACK_EX + "DIGITE O NOVO NOME DO ALUNO(A): " + Style.RESET_ALL)
+                novo_nome = input(Fore.LIGHTBLACK_EX + "DIGITE O NOVO NOME DO ALUNO(A): \n" + Style.RESET_ALL)
                 if novo_nome.replace(" ", "").isalpha():
                     break
                 elif novo_nome ==" ":
-                            print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS" + Style.RESET_ALL)
+                            print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS\n" + Style.RESET_ALL)
                 else:
-                    print(Fore.YELLOW + "DIGITE APENAS LETRAS" + Style.RESET_ALL)
+                    print(Fore.YELLOW + "DIGITE APENAS LETRAS\n" + Style.RESET_ALL)
 
             while True:
                 try:
-                    nova_nota = float(input(Fore.LIGHTBLACK_EX + "DIGITE A NOVA NOTA DO ALUNO(A): " + Style.RESET_ALL).replace(",","."))
+                    nova_nota = float(input(Fore.LIGHTBLACK_EX + "DIGITE A NOVA NOTA DO ALUNO(A): \n" + Style.RESET_ALL).replace(",","."))
                     
                     if 0 <= nova_nota <= 10:
                         break
                     if novo_nome.replace(" ", "").isalpha():
                                         break
                     elif nova_nota ==" ":
-                        print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS" + Style.RESET_ALL)
+                        print(Fore.YELLOW + "NÃO PODE ESPAÇOS VAZIOS\n" + Style.RESET_ALL)
                     else:
-                            print(Fore.YELLOW + "DIGITE APENAS NÚMEROS ENTRE 0 E 10" + Style.RESET_ALL)
+                            print(Fore.YELLOW + "DIGITE APENAS NÚMEROS ENTRE 0 E 10\n" + Style.RESET_ALL)
                 except ValueError:
-                    print(Fore.YELLOW + "DIGITE APENAS NÚMEROS" + Style.RESET_ALL)
+                    print(Fore.YELLOW + "DIGITE APENAS NÚMEROS\n" + Style.RESET_ALL)
 
             nova_lista.append(f"{novo_nome};{nova_nota}\n")
         else:
             nova_lista.append(linha)
 
     if not encontrado:
-        print(Fore.RED + "Aluno não encontrado!" + Style.RESET_ALL)
+        print(Fore.RED + "Aluno não encontrado!\n" + Style.RESET_ALL)
         return
 
     with open(ARQUIVO, "w", encoding="utf-8") as arquivo:
         arquivo.writelines(nova_lista)
 
-    print(Fore.GREEN + "Aluno(a) alterado com sucesso!" + Style.RESET_ALL)
+    print(Fore.GREEN + "Aluno(a) alterado com sucesso!\n" + Style.RESET_ALL)
 
 
 def excluir():
     titulo("EXCLUIR ALUNO(A)")
 
     if not os.path.exists(ARQUIVO):
-        print(Fore.RED + "Arquivo de cadastro inexistente!" + Style.RESET_ALL)
+        print(Fore.RED + "Arquivo de cadastro inexistente!\n" + Style.RESET_ALL)
         return
 
-    aluno = input("Digite o nome do aluno(a): ").strip()
+    aluno = input("Digite o nome do aluno(a): \n").strip()
 
     with open(ARQUIVO, "r", encoding="utf-8") as arquivo:
         alunos = arquivo.readlines()
@@ -161,13 +161,13 @@ def excluir():
             nova_lista.append(linha)
 
     if not encontrado:
-        print(Fore.RED + "Aluno(a) não encontrado!" + Style.RESET_ALL)
+        print(Fore.RED + "Aluno(a) não encontrado!\n" + Style.RESET_ALL)
         return
 
     with open(ARQUIVO, "w", encoding="utf-8") as arquivo:
         arquivo.writelines(nova_lista)
 
-    print(Fore.GREEN + "Aluno(a) excluído com sucesso!" + Style.RESET_ALL)
+    print(Fore.GREEN + "Aluno(a) excluído com sucesso!\n" + Style.RESET_ALL)
 
 
 while True:
@@ -194,8 +194,8 @@ while True:
         excluir()
 
     elif opcao == "5":
-        print(Fore.YELLOW + "Programa encerrado." + Style.RESET_ALL)
+        print(Fore.YELLOW + "Programa encerrado.\n" + Style.RESET_ALL)
         break
 
     else:
-        print(Fore.RED + "Opção inválida!" + Style.RESET_ALL)
+        print(Fore.RED + "Opção inválida!\n" + Style.RESET_ALL)
